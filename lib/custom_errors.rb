b843def8098f2f8e36bca37a63a36a7f158378cf
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-08-06 23:06:59 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,15 +1,18 @@
 class Person
-  attr_accessor :partner, :name
+  class PartnerError<StandardError
+    
+    attr_accessor :partner, :name
 
-  def initialize(name)
-    @name = name
+    def initialize(name)
+      @name = name
+    end
+
+    def get_married(person)
+      self.partner = person
+      person.partner = self
+    end
+
   end
-
-  def get_married(person)
-    self.partner = person
-    person.partner = self
-  end
-
 end
 
 beyonce = Person.new("Beyonce")
